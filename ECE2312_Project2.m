ecd707840beda0085f51ea42d6df6efd0eb092cc
Jacob Nguyen <123837162+jvnguyen102@users.noreply.github.com>
2023-02-17 14:43:12 -0500

    Update ECE2312_Project2.m

--- ECE2312_Project2.m
+++ ECE2312_Project2.m
@@ -15,7 +15,7 @@ n = 0:1/fs:duration;
 sine_wave = sin(2*pi*5000*n);
 sound(sine_wave,fs);
 
-%  WAV file for sine_wave
+% % WAV file for sine_wave
 WAVsave('team[2]-sinetone.wav', sine_wave, fs);
 [y,fs] = audioread('team[2]-sinetone.wav');
 
@@ -26,15 +26,12 @@ plotSpec(y, fs, 'team[2]-sinetone.wav');
 chirp_sine = sin(2*pi*(n.*800.*n));
 soundsc(chirp_sine, fs)
 
-% % WAV file for chirp_wave
-% WAVsave('team[2]-chirp.wav', chirp_sine, fs);
-% [y,fs] = audioread('team[2]-chirp.wav');
-% 
-% % Spectrogram for WAV file chirp_sine
-% plotSpec(y, fs, 'team[2]-chirp.wav');
+% WAV file for chirp_wave
+WAVsave('team[2]-chirp.wav', chirp_sine, fs);
+[ych,fs] = audioread('team[2]-chirp.wav');
 
-% % Close Encounters of the Third Kind Notes
-% % cetk_sine
+% Spectrogram for WAV file chirp_sine
+plotSpec(ych, fs, 'team[2]-chirp.wav');
 
 cetk_values = [4000 4500 3500 2500 3000];
 cetk_durations = [0.25 0.5 1 0.5 2.75];
@@ -55,9 +52,8 @@ plotSpec(yc, fs, 'team[2]-cetk.wav');
 end
 
 % Combining Sound Files
-[y,fs] = audioread('team[2]-sinetone.wav'); % 5000 Hz sine tone
 [y2,fs] = audioread('Recording_1.wav'); % Previously recorded speech file from Project 1
-y = y(1:numel(y2)); % Ensure both signals are of equal length
+y = y(1:numel(y2));
 combined_signal = (y+y2)/2;
 sound(combined_signal,fs);
 pause(duration);
@@ -87,7 +83,4 @@ WAVsave('team[2]-stereospeechsine.wav', stereo_signal, fs);
 
 % Spectrograms of both Stereo Channels
 plotSpec(combined, fs, 'Speech Signal + Sine Tone');
-
 plotSpec(y2, fs, 'Speech Signal');
-
-
